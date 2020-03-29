830e9c7ffa59e401e69a5f0058ea5e46839bf041
MaxWang <62160459+ailinwang0501@users.noreply.github.com>
2020-03-29 22:43:56 +0800

    forth commit

--- src/main/java/com/maxwang/life/work/controller/IndexController.java
+++ src/main/java/com/maxwang/life/work/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.maxwang.life.work.controller;
 
+import com.maxwang.life.work.mapper.UserMapper;
+import com.maxwang.life.work.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
@@ -12,8 +18,23 @@ public class IndexController {
         model.addAttribute("name", name);
         return "index";
     }*/
-    @GetMapping("/index")
-    public String index(){
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) { //输入 cookies.for 自动匹配格式
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token); //alt + enter 创建方法 一直回车 会以驱动编程方式创建方法
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        //request.getSession().setAttribute("user", user);
         return "index";
     }
 }
