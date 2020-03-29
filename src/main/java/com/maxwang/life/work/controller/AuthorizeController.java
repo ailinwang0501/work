4b4545082976e7473d09ea3a9dc556bb3ca4526e
MaxWang <62160459+ailinwang0501@users.noreply.github.com>
2020-03-29 20:49:31 +0800

    forth commit

--- src/main/java/com/maxwang/life/work/controller/AuthorizeController.java
+++ src/main/java/com/maxwang/life/work/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.maxwang.life.work.controller;
 
 import com.maxwang.life.work.dto.AccessTokenDTO;
 import com.maxwang.life.work.dto.GithubUser;
+import com.maxwang.life.work.mapper.UserMapper;
+import com.maxwang.life.work.model.User;
 import com.maxwang.life.work.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,6 +27,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(
             @RequestParam(name="code") String code,
@@ -36,14 +42,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
-            request.getSession().setAttribute("user", user);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
             //log success write session and cookie
         }else {
-            return "redirect:/";
             //log fail relogin
         }
+        return "redirect:/";
     }
 }
