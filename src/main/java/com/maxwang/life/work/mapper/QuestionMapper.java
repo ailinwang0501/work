163924fc36cfe5ae62f114c3e663fa4bf8570878
MaxWang <62160459+ailinwang0501@users.noreply.github.com>
2020-04-01 23:40:33 +0800

    sixth commit

---
+++ src/main/java/com/maxwang/life/work/mapper/QuestionMapper.java
@@ -0,0 +1,14 @@
+package com.maxwang.life.work.mapper;
+
+
+import com.maxwang.life.work.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (name,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
+
+
